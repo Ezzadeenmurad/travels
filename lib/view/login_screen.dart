ea3dbbba459bd250fc6e0999e954e27b9623baf1
Ezzadeenmurad <ezzaldeenmorad@gmail.com>
2023-04-11 02:55:59 +0300

    modify inkweel&button

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_Container.dart';
 import '../widgets/custom_text_form_shadw.dart';
+import '../widgets/custombutton.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -36,6 +38,91 @@ class _LoginScreenState extends State<LoginScreen> {
                   height: 15,
                 ),
                 customTextFieldShadow(hint: "كلمة المرور"),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: (() {}),
+                      child: const Text(
+                        "نسيت كلمة المرور",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        const Text(
+                          "تذكرني",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Checkbox(
+                          value: false,
+                          onChanged: (value) {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: (() {}),
+                      child: const Text(
+                        "انشاء حساب جديد",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Text(
+                      'ليس لديك حساب ؟',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                customContainer(
+                  child: TypeButton.elevatedButton(
+                    onPressed: () {},
+                    text: "دخول",
+                    buttonColor: Color(0xffff474f),
+                  ),
+                  containerColor: Colors.white,
+                  width: double.infinity,
+                  height: 60,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                customContainer(
+                  child: TypeButton.outlinedButton(
+                    onPressed: () {},
+                    text: "تخطي",
+                  ),
+                  height: 60,
+                  width: double.infinity,
+                ),
               ],
             ),
           ],
