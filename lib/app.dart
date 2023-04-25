8b72fb81faab85b4797d129fdfae13ae69ab136b
Ezzadeenmurad <ezzaldeenmorad@gmail.com>
2023-04-25 20:30:32 +0300

    modify app

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:travels/view/login_screen.dart';
+
+import '../core/utils/Bindings.dart';
 
 import 'view/home_screen.dart';
+import 'view/login_screen.dart';
 
 class Travels extends StatelessWidget {
   const Travels({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home:HomeScreen (),
+      initialBinding: MyBindings(),
     );
   }
 }
